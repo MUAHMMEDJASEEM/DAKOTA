abe107d02b2e54f3f04a26acd848b9a69d7b7b4e
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2023-01-14 11:24:25 +0530

    Update Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,4 @@
-import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
-import React from 'https://esm.sh/react@18.2.0'
+import React from 'react';
 import './cal.css';
 class Calculator extends React.Component {
   constructor() {
