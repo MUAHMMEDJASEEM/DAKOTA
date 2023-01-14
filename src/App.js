3c512c0ef5dc043d9ab6ab9a9f3e597df93a9f40
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2023-01-14 10:44:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>dakota</h1>
+      <ul>
+        <li>happy  luqman</li>
+        <li>cutty shahan</li>
+        <li>motivated  haneen</li>
+        <li>apple watch faiz</li>
+        <li>nattil pok favas</li>
+        <li>strict hisham</li>
+        <li>nan thanne jaseem</li>
+      </ul>
     </div>
   );
 }
